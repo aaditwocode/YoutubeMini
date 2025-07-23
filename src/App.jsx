2104a6f837c784daae563913319f135027418591
Aaditya Pratap Singh <aaditwocode@gmail.com>
2025-07-23 13:30:36 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './utils/store'; 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainContainer from './components/MainContainer';
-import WatchPage from './components/Watchpage';
+import WatchPage from './components/WatchPage';
 
 const appRouter = createBrowserRouter([
   {
